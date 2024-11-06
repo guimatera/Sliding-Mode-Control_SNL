90ea2eda123bb6e39514bc8bbfcfed0a6193b478
Guilherme Matera <guimatera@gmail.com>
2024-11-06 16:22:42 -0300

    feature: Criação do efeito de uma dinâmica não modelada de primeira ordem de ganho estático unitário no sistema

--- SlidingModeControl_simulink_export.m
+++ SlidingModeControl_simulink_export.m
@@ -1,7 +1,7 @@
 clc
 
 % Condições iniciais
-y0 = [10;0]; % y0(1) = Y(0); y0(2) = Y'(0)
+y0 = [2;2]; % y0(1) = Y(0); y0(2) = Y'(0)
 
 % Parâmetros da lei de controle por modo deslizante
 g = 1;
@@ -9,7 +9,10 @@ alpha = 1;
 K = 1;
 
 % Parâmetros de perturbação
-t_inicial = 0;
-t_final = 5;
+t_inicial = 3;
+t_final = 8;
 amplitude = 1;
+
+% Ganho estático unitário de uma dinâmica não modelada de primeira ordem
+a = 2;
 
