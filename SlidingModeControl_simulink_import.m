c9fbedcc0d4f212adb393ca85814eba948a46ade
Guilherme Matera <guimatera@gmail.com>
2024-11-06 16:21:48 -0300

    refactor: Melhorias na visualização e ordenação de gráficos.

--- SlidingModeControl_simulink_import.m
+++ SlidingModeControl_simulink_import.m
@@ -1,39 +1,40 @@
 close all
 
+
+x = linspace(-(max(y0)+1),max(y0)+1);
+
 % Gráfico do sinal de controle pelo tempo
 figure(1)
 plot(out.v)
 grid
 title('Sinal de controle x Tempo')
 
-x = linspace(-(max(y0)+1),max(y0)+1);
-
-% Gráfico do sinal da derivada de Y por Y (plano de fase) e lugar
-% geométrico
+% Gráfico do sinal da derivada de Y pelo tempo
 figure(2)
-plot(out.Y.Data,out.Ydot.Data)
-hold on
-plot(x,-alpha*x,'b')
-hold on
-plot(y0(1), y0(2), 'ro', 'MarkerSize', 5, 'MarkerFaceColor', 'r');
+plot(out.Ydot.Time,out.Ydot.Data)
 ylabel('Ydot');
-xlabel('Y')
-title('Ydot x Y')
+xlabel('T(s)')
+title('Ydot x t')
 grid on;
 
 % Gráfico do sinal de Y pelo tempo
 figure(3)
 plot(out.Y.Time,out.Y.Data)
-ylabel('Ydot');
-xlabel('Y')
+ylabel('Y');
+xlabel('T(s)')
 title('Y x t')
 grid on;
 
-% Gráfico do sinal da derivada de Y pelo tempo
+% Gráfico do sinal da derivada de Y por Y (plano de fase) e lugar
+% geométrico
 figure(4)
-plot(out.Ydot.Time,out.Ydot.Data)
+plot(out.Y.Data,out.Ydot.Data,'Color','r')
+hold on
+plot(x,-alpha*x,'--','Color','k')
+hold on
+plot(y0(1), y0(2), 'ro', 'MarkerSize', 5, 'MarkerFaceColor', 'g');
 ylabel('Ydot');
-xlabel('T(s)')
-title('Ydot x t')
+xlabel('Y')
+title('Ydot x Y')
 grid on;
 
